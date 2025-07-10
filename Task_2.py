b73a0db1857092f65f7db595c1aad5011f82bc77
arnav2k4 <161723756+arnav2k4@users.noreply.github.com>
2025-07-10 12:36:30 +0530

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,8 +1,8 @@
 L = []
 for i in range(1,11):
     L.append(i)
-
+print("Original List: ",L)
 L5 = L[0:5]
-print(L5)
+print("Extracted first five elements: ",L5)
 L5.reverse()
-print(L5)
+print("Reversed extract elements: ",L5)
